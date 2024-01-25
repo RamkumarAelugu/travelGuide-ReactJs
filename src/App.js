4d9ceb4d427f7979fafd7c3d5469598d4f9cc165
RamkumarAelugu <ramkumaraelugu@gmail.com>
2024-01-25 08:45:41 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import TravelGuide from './components/TravelGuide'
+import './App.css'
+
+// Replace your code here
+
+class App extends Component {
+  state = {isLoading: true, totalDataList: []}
+
+  componentDidMount() {
+    this.getTravelGuideDetails()
+  }
+
+  getTravelGuideDetails = async () => {
+    const url = 'https://apis.ccbp.in/tg/packages'
+    const options = {
+      method: 'GET',
+    }
+
+    const response = await fetch(url, options)
+    if (response.ok === true) {
+      const data = await response.json()
+      const formattedData = data.packages.map(each => ({
+        id: each.id,
+        imageUrl: each.image_url,
+        name: each.name,
+        description: each.description,
+      }))
+      this.setState({isLoading: false, totalDataList: formattedData})
+    }
+  }
+
+  renderLoadingViewContainer = () => (
+    <div data-testId="loader" className="loader-container">
+      <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+    </div>
+  )
+
+  renderSuccessViewContainer = () => {
+    const {totalDataList} = this.state
+    return (
+      <ul className="list-container">
+        {totalDataList.map(e => (
+          <TravelGuide key={e.id} travelGuideDetails={e} />
+        ))}
+      </ul>
+    )
+  }
+
+  render() {
+    const {isLoading} = this.state
+
+    return (
+      <div className="main-container">
+        <h1 className="main-heading">Travel Guide</h1>
+        <div className="container">
+          {isLoading
+            ? this.renderLoadingViewContainer()
+            : this.renderSuccessViewContainer()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
